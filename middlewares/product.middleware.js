b0e1aaefd1e5502e8a6b3b83758363f262cee628
BouzirJawad <jawadbouzir01@gmail.com>
2025-08-11 10:59:50 +0100

    input validation middleware added

---
+++ middlewares/product.middleware.js
@@ -0,0 +1,10 @@
+const { body } = require("express-validator")
+
+exports.validateProduct = [
+    body("name").isString().isLength({ min: 3, max: 50 }).withMessage("Name must be at lease 3 characters (max 50)"),
+    body("description").isLength({ min: 10, max: 1000 }).withMessage("Description must be at least 10 characters (max 1000)"),
+    body("price").isFloat({ min: 0 }).withMessage("Proce must be a positive number"),
+    body("category").isIn(["machines", "tools", "materials"]).withMessage("Invalid category"),
+    body("imageUrl").isURL().withMessage("Image must be a valid URL"), 
+    body("stock").isInt({ min: 0 }).withMessage("Stock must be a positive number")
+]
